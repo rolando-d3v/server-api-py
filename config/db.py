1a65ddf4d3514b749c5acf78ccd0737d9b425819
rolando-d3v <landon3388@gmail.com>
2024-05-24 22:13:33 -0500

    sds

--- config/db.py
+++ config/db.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, MetaData
+# from sqlalchemy import create_engine, MetaData
 
-create_engine("")
+# create_engine("")
 
--- main.py
+++ main.py
@@ -1,41 +1,22 @@
-from smartcard.System import readers
-from smartcard.util import toHexString
-from smartcard.CardConnection import CardConnection
-from smartcard.Exceptions import CardConnectionException
+from fastapi import FastAPI
+from typing import Union
+from api.user import user_router
 
-# Obtener la lista de lectores
-r = readers()
-if not r:
-    print("No se encontraron lectores de tarjetas inteligentes")
-    exit()
+app = FastAPI()
 
-# Seleccionar el primer lector
-reader = r[0]
+# app.include_router(api.router, prefix="/api")
+app.include_router(user_router.user, prefix="/api")
 
-try:
-    # Conectar al lector
-    connection = reader.createConnection()
-    connection.connect()
 
-    # Comando APDU para leer el número de DNI
-    apdu_command = [0xa0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0xc, 0x01, 0x01]
-    # apdu_command = [0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x00, 0x54, 0x48, 0x01, 0x00]
 
-    # Enviar el comando APDU
-    data, sw1, sw2 = connection.transmit(apdu_command)
-    print(data)
-    print(sw1)
-    print(0x11)
-    print(0xFF)
-    print(0xFF)
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
 
-    # Verificar la respuesta
-    if sw1 == 0x11 and sw2 == 0x00:
-        # Extraer el número de DNI de los datos recibidos
-        dni_number = toHexString(data)[-8:].decode('utf-8')
-        print("Número de DNI:", dni_number)
-    else:
-        print("Error al leer el número de DNI:", sw1, sw2)
+
+# @app.get("/items/{item_id}")
+# def read_item(item_id: int, q: Union[str, None] = None):
+#     return {"item_id": item_id, "q": q}
 
 
 
@@ -43,11 +24,59 @@ try:
 
 
 
-except CardConnectionException as e:
-    print("Error de conexión:", e)
 
-finally:
-    # Desconectar del lector
-    connection.disconnect()
+# from smartcard.System import readers
+# from smartcard.util import toHexString
+# from smartcard.CardConnection import CardConnection
+# from smartcard.Exceptions import CardConnectionException
+
+# # Obtener la lista de lectores
+# r = readers()
+# if not r:
+#     print("No se encontraron lectores de tarjetas inteligentes")
+#     exit()
+
+# # Seleccionar el primer lector
+# reader = r[0]
+
+# try:
+#     # Conectar al lector
+#     connection = reader.createConnection()
+#     connection.connect()
+
+#     # Comando APDU para leer el número de DNI
+#     apdu_command = [0xa0, 0x00, 0x00, 0x00, 0x62, 0x03, 0x01, 0xc, 0x01, 0x01]
+#     # apdu_command = [0x00, 0xA4, 0x04, 0x00, 0x08, 0xA0, 0x00, 0x00, 0x00, 0x54, 0x48, 0x01, 0x00]
+
+#     # Enviar el comando APDU
+#     data, sw1, sw2 = connection.transmit(apdu_command)
+#     print(data)
+#     print(sw1)
+#     print(0x11)
+#     print(0xFF)
+#     print(0xFF)
+
+#     # Verificar la respuesta
+#     if sw1 == 0x11 and sw2 == 0x00:
+#         # Extraer el número de DNI de los datos recibidos
+#         dni_number = toHexString(data)[-8:].decode('utf-8')
+#         print("Número de DNI:", dni_number)
+#     else:
+#         print("Error al leer el número de DNI:", sw1, sw2)
+
+
+
+
+
+
+
+# except CardConnectionException as e:
+#     print("Error de conexión:", e)
+
+# finally:
+#     # Desconectar del lector
+#     connection.disconnect()
+
+
 
 
